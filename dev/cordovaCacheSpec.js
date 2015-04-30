57f7c5fa3b25fdc3af1268d7b9027005c659f39e
trojanspike <trojan_spikepk@yahoo.co.uk>
2015-04-30 17:21:45 +0100

    add tests

---
+++ dev/cordovaCacheSpec.js
@@ -0,0 +1,35 @@
+describe("Checks something cool", function() {
+  return it("checks window is an object", function() {
+    return expect(window).toBeDefined();
+  });
+});
+
+describe("Checks something cool", function() {
+  return it("checks window is an object", function() {
+    return expect(window).toBeDefined();
+  });
+});
+
+describe("Checks something cool", function() {
+  return it("checks window is an object", function() {
+    return expect(window).toBeDefined();
+  });
+});
+
+describe("Checks something cool", function() {
+  return it("checks window is an object", function() {
+    return expect(window).toBeDefined();
+  });
+});
+
+
+/*
+   document.addEventListener 'deviceready', ->
+  SI.cordovaCache 'SomeThing' , (Cache, Crypt)->
+
+
+  expect( typeof window.SI ).toBe("object");
+  expect( typeof CryptoJS.AES ).toBe("object");
+  expect( typeof window.requestFileSystem ).toBe("function");
+  expect( typeof Crypt ).toBe("object");
+ */
---
+++ dev/specs/00-Name-Spec.coffee
@@ -0,0 +1,34 @@
+# http://jasmine.github.io/2.1/introduction.html
+
+describe "Checks something cool", ->
+  it "checks window is an object", ->
+    expect window
+    .toBeDefined()
+
+describe "Checks something cool", ->
+  it "checks window is an object", ->
+    expect window
+    .toBeDefined()
+
+
+describe "Checks something cool", ->
+  it "checks window is an object", ->
+    expect window
+    .toBeDefined()
+
+describe "Checks something cool", ->
+  it "checks window is an object", ->
+    expect window
+    .toBeDefined()
+
+
+###
+   document.addEventListener 'deviceready', ->
+  SI.cordovaCache 'SomeThing' , (Cache, Crypt)->
+
+
+  expect( typeof window.SI ).toBe("object");
+  expect( typeof CryptoJS.AES ).toBe("object");
+  expect( typeof window.requestFileSystem ).toBe("function");
+  expect( typeof Crypt ).toBe("object");
+###
