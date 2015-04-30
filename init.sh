1c5f6a01d19d74b9a5c142a62927acb5e25bd8c9
trojanspike <trojan_spikepk@yahoo.co.uk>
2015-04-30 18:51:55 +0100

    initiate & conf

--- init.sh
+++ init.sh
@@ -1,24 +1,6 @@
 #!/usr/bin/env bash
 
-BASE=$(pwd)
+# https://git-wip-us.apache.org/repos/asf/cordova-plugin-file.git
 
-# Do some init install for dev env
-echo "Running init install of node module"
-sleep 2
-npm install && \
-\
-# Make cordova dir structure not saved in git
-echo "Creating cordova dir structure"
-sleep 2
-mkdir $BASE/test/{hooks,platforms,plugins} && \
-\
-\
-echo "Setting up the cordova app"
-sleep 2
+npm install
 
-cd  $BASE/test && cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-file.git && \
-git clone https://github.com/trojanspike/CordovaAndroidCrosswalkInstall.git CrossWalk && \
-\
-mv $BASE/test/CrossWalk/CrossWalkInstall.sh $BASE/test/ && rm -Rf $BASE/test/CrossWalk/ && \
-\
-$BASE/test/CrossWalkInstall.sh && unlink $BASE/test/CrossWalkInstall.sh
