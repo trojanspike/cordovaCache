d1dc7884e3b52511eedf72fb2da943b97b1153fb
trojanspike <trojan_spikepk@yahoo.co.uk>
2015-05-01 14:01:46 +0100

    Test app create script installing into current folder

--- test/CreateTestApp.sh
+++ test/CreateTestApp.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/bash
 
 BASE=$(pwd)
+clear
+echo "Installing bower components" && sleep 2 && \
+bower install
+
+echo "setting up the test app" && sleep 2 && \
 
 mkdir $BASE/{hooks,platforms,plugins} && cordova platform add android && cordova plugin add org.apache.cordova.file && \
+\
+clear && echo "Installing CrossWalk browser" && sleep 2 && \
+\
 git clone https://github.com/trojanspike/CordovaAndroidCrosswalkInstall.git CrossWalk && \
 \
 mv $BASE/CrossWalk/CrossWalkInstall.sh $BASE/ && rm -Rf $BASE/CrossWalk/ && \
 $BASE/CrossWalkInstall.sh && unlink $BASE/CrossWalkInstall.sh && cp -r $BASE/AndroidManifest.xml $BASE/platforms/android/ && clear && \
-echo " Test App Created "
+echo " Test App Created , Ensure /www/index.html is using the correct ip address for accessing the local javascript."
